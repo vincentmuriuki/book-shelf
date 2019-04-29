bbbf922a944bcbaab1c093b4993d69e3c68a0601
vincentmuriuki <vincentmuriuki42@gmail.com>
2019-04-29 23:30:44 +0300

    UI development

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomeContainer from '../../containers/home_container'
 
 const Home = (props) => {
     return (
         <div>
-            
+            <HomeContainer/>
         </div>
     )
 };
